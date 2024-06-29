103792d40e717e5903cf369e27096fdad6fa2636
pawan <pawanpoudel69@gmail.com>
2024-06-29 01:49:31 +0000

    keyword

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,9 +7,10 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Pawan Poudel",
-  description: "A passionate full-stack web developer with a focus on crafting engaging and functional web applications.",
-  keywords:"pawan,pawan poudel"
+  description: "Pawan Poudel is a passionate full-stack web developer with a focus on crafting engaging and functional web applications.",
+  keywords:"pawan, pawan poudel, pawan-poudel, pawan_poudel, pawan poudel portfolio, pawan poudel website, pawan poudel google, pawan poudel bing, pawan poudel social"
 };
+
 
 export default function RootLayout({
   children,
@@ -18,6 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <link rel="canonical" href="https://pawan-poudel.com.np"/>
       <body className={inter.className}>
     <div className="min-h-screen w-full bg-dot-white/[0.1] relative ">
 
